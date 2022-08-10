3cd73c0e9b1a4fe3e19e2a65f89b3d6836fcbdad
Gonzalo Carmona López <gonzalo.carmona@bosonit.com>
2022-08-10 13:11:26 +0200

    enable iterative imputer

--- titanic/training/train.py
+++ titanic/training/train.py
@@ -25,6 +25,7 @@ POSSIBILITY OF SUCH DAMAGE.
 """
 from sklearn.metrics import precision_score
 from sklearn.model_selection import train_test_split
+from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import KNNImputer, SimpleImputer, IterativeImputer
 from sklearn.pipeline import make_pipeline
 from sklearn.compose import make_column_transformer
