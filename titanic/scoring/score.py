3085ad2105190f2998f19f0d298a26674996e41d
Gonzalo Carmona López <gonzalo.carmona@bosonit.com>
2022-08-16 11:57:11 +0200

    path local en score.py

--- titanic/scoring/score.py
+++ titanic/scoring/score.py
@@ -54,14 +54,14 @@ def remove_columns(X_data):  # Elimina columnas que no usaremos en el modelo
 def init():
     # load the model from file into a global object
     global model
-
+    
     # we assume that we have just one model
     # AZUREML_MODEL_DIR is an environment variable created during deployment.
     # It is the path to the model folder
     # (./azureml-models/$MODEL_NAME/$VERSION)
-    model_path = Model.get_model_path(
-        os.getenv("AZUREML_MODEL_DIR").split('/')[-2])
-
+    #  model_path = Model.get_model_path(
+    #    os.getenv("AZUREML_MODEL_DIR").split('/')[-2])
+    model_path = "C:/Users/gonzalo.carmona/Desktop/modelo/titanic_model.pkl"
     model = joblib.load(model_path)
 
 
